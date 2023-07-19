02934a807d92b4aa52975cb7af2d5f9dc97ab638
emmanuel nnaji <realreach@yahoo.com>
2023-07-19 19:13:24 +0000

    file that prints the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,56 +5,75 @@
  * Return: nothing
  */
 
+
 void times_table(void)
 {
-	int a0, a1, a2, a3, a4, a5, a6 a7, a8, a9;
+	int m, n, val;
 
-	for (a0 = '0'; a0 >= '0' && a0 <= '9'; a0++)
-	{
-		
-		_putchar(0 * a0);
-	}
-	for (a1 = '0'; a1 >= '0' && a1 <= '9'; a1++)
-	{
-		_putchar(1 * a1);
-	}
+	val = 0;
 
-	for (a2 = '0'; a2 >= '0' && a2 <= '9'; a2++)
+	for (m = 0; m >= 0 && m <= 9; m++)
 	{
-		_putchar(2 * a2);
-	}
+		for (n = 0; n >= 0 && n <= 9; n++)
+		{
+			val = m * n;
 
-	for (a3 = '0'; a3 >= '0' && a3 <= '9'; a3++)
-	{
-		_putchar(3 * a3);
-	}
+			if (val > 9)
+			{
+				int rev, count, digit;
 
-	for (a4 = '0'; a4 >= '0' && a4 <= '9'; a4++)
-	{
-		_putchar(4 * a4);
-	}
+				rev = 0;
+				count = 0;
+				digit = 0;
 
-	for (a5 = '0'; a5 >= '0' && a5 <= '9'; a5++)
-	{
-		_putchar(5 * a5);
-	}
+				/** 
+				 * pick out numbers from long int in reverse order begining with
+				 * the last for ease of print by _putchar
+				 */
+				while (val > 0)
+				{
+					digit = val % 10;
 
-	for (a6 = '0'; a6 >= '0' && a6 <= '9'; a6++)
-	{
-		_putchar(6 * a6);
-	}
+					if (digit == 0)
+						count = 1;
 
-	for (a7 = '0'; a7 >= '0' && a7 <= '9'; a7++)
-	{
-		_putchar(7 * a7);
-	}
+					rev = rev * 10 + digit;
+					val /= 10;
+				}
 
-	for (a8 = '0'; a8 >= '0' && a8 <= '9'; a8++)
-	{
-		_putchar(8 * a8);
-	}
+				/* print reversed number */
+				_putchar(' ');  /* to alighn right */
+				while (rev > 0)
+				{
+					_putchar('0' + rev % 10);
+					rev /= 10;
+				}
 
-	for (a9 = '0'; a9 >= '0' && a9 <= '9'; a9++)
-	{
-		_putchar(9 * a9);
+				/* formsake of leading zero */
+				if (count)
+					_putchar('0');
+
+				if (n != 9)	/* to avoid comma at end of table */
+				_putchar(',');
+			}
+			else
+			{
+				/**
+				 * the space has 2come b4 the value; to alighn right
+				 * but not b4 the initial column
+				 */
+				if (n != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(val + '0');
+
+				/* to avoid comma at the end of table */
+				if (n != 9)
+					_putchar(',');
+			}
+		}
+		_putchar('\n');
 	}
+}
