65efdd317280d5aced421b853db18fac8b852c90
emmanuel nnaji <realreach@yahoo.com>
2023-07-18 23:08:59 -0700

    prints every second of 24hrs

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minutes of the day
+ * Return: nothing
+ */
+
+void jack_bauer(void)
+{
+	int a, b, c, d, e;
+
+	for (a = 0; a > = 0 && a <= 23; a++)
+	{
+		for (a = 0; a > = 0 && a <= 23; a++)
+		{
+			for (a = 0; a > = 0 && a <= 23; a++)
+			{
+				for (a = 0; a > = 0 && a <= 23; a++)
+				{
+					for (a = 0; a > = 0 && a <= 23; a++)
+					{
+
