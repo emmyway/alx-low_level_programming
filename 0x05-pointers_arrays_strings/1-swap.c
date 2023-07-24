41e0bafe9a553a1ff785d4b0a1880ef064474f78
emmanuel nnaji <realreach@yahoo.com>
2023-07-24 14:34:54 -0700

    file that swaps two pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of two pointers
+ * @a: the 1st pointer parameter
+ * @b: the 2nd pointer parameter
+ */
+
+void swap_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}
