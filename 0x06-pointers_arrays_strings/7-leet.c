131696e6dac9bf6b2d63b90f9f13ee26bd414b85
emmanuel nnaji <realreach@yahoo.com>
2023-07-28 09:09:07 +0000

    file that replace some xters with integers in a strring

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,22 +1,26 @@
 #include "main.h"
 
 /**
- * leet- 
+ * leet - converts some letters to integers in a string
  * @str: the string
  * Return: pointer to changed string
  */
 
-char *leet(char *)
+char *leet(char *str)
 {
 	int m, n;
+	/*char *s1, *s2;*/
+
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
 	m = 0;
 	while (str[m])
 	{
-		for (n = 'a'; n >= 'a' && n <= 'z'; n++)
+		for (n = 0; n >= 0 && n < 10; n++)
 		{
-			if (str[m] == n)
-				str[m] = str[m] - 32;
+			if (str[m] == s1[n])
+				str[m] = s2[n];
 		}
 		m++;
 	}
