441c36647d6fda666c3c7ec021890b6c54c9faa1
emmanuel nnaji <realreach@yahoo.com>
2023-07-30 07:15:17 +0000

    file converts string xters to integer values

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _atoi - conversts string to integeras atoi
+ * @s: the string to be conbverted
+ * Return: the converted integer
+ */
+
+
+int _atoi(char *s)
+{
+	int i, t, len, num, sign;
+
+	len = 0;
+	while (s[len])
+		len++;
+
+	num = 0;
+	sign = 1;
+
+	for (i = 0; i < len; i++)
+	{
+
+		if (s[i] == ' ')
+                {
+                        i++;
+			sign = 1;
+                }
+
+		if (s[i] == '+')
+			sign = 1 * sign;
+		else if (s[i] == '-')
+			sign = -1 * sign;
+
+		for (t = 48; t >= 48 && t <= 57; t++)
+		{
+			if (s[i] == t)
+				num = num * 10 + (t - 48);
+			continue;
+		}
+	}
+	return (num * sign);
+}
