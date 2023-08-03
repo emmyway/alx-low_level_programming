05f1c541b58fd16efdd5bc426a78e9b218f81326
emmanuel nnaji <realreach@yahoo.com>
2023-08-03 19:43:47 +0000

    file finds factorial of number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,12 +2,13 @@
 
 /**
  * factorial - prints factorial of a number
- * @n: the the number to be calculate
+ * @n: the number to be calculate
+ * Return: factorial value
  */
 
 int factorial(int n)
 {
-	if ( n < 0)
+	if (n < 0)
 		return (-1);
 
 	if (n == 0)
