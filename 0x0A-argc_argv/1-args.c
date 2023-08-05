c19432e4b409fcdbf936326fda0cbebaf5bf7f0e
emmanuel nnaji <realreach@yahoo.com>
2023-08-04 22:03:04 -0700

    created files

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry of prog
+ * Return: 0 on success
+ */
+
+int main(int argc, argv[0])
+{
+
+	printf("%d\n", argc);
+
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,1 +1,1 @@
-0-whatsmyname.c
+2-args.c, 3-mul.c 4-add.c 100-change.c
