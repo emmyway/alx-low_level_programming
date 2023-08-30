8cf221b749aa7664cb712012b9a1bfb228bde8e0
emmanuel nnaji <realreach@yahoo.com>
2023-08-29 20:44:51 -0700

    singly linked list tasks

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *ptr, *temp;
 
-	temp = malloc(listint_t);
+	temp = malloc(sizeof(listint_t));
 	if (temp == 0)
 		return (NULL);
 
