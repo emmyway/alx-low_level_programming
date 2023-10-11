a685d9debb0398509ca0026c6508f9b98ff04934
emmanuel nnaji <realreach@yahoo.com>
2023-10-11 01:16:51 +0000

    macro definition for computation of absolute value of number

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef MACROLIKE_H
+#define MACROLIKE_H
+
+/**
+ * File: 3-function_like_ macro
+ * @x: the arg value
+ * Description: header file
+ */
+#define ABS(x) ((x) < 0 ? -1 * (x) : (x))
+
+#endif
