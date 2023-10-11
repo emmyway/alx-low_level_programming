f4f6eb0179f6d42f6e83cac91c51da72507bff6c
emmanuel nnaji <realreach@yahoo.com>
2023-10-11 01:25:03 +0000

    macro definition for computation of sum of two int value

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * File: 4-sum
+ * @x: the first int arg value
+ * @y: the second int arg value
+ * Description: header file
+ */
+#define SUM(x, y) (x + y)
+
+#endif
