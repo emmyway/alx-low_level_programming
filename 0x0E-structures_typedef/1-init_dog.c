106600cd7c2ce8dfa15c88d1bb643a00e518c161
emmanuel nnaji <realreach@yahoo.com>
2023-10-11 02:04:43 +0000

    fxn initialises a struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - fxn initializes dog struct
+ * @d: pointer to struct dog
+ * @name: name of new dog
+ * @age: age of dog
+ * @owner: name of owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+5-free_dog.c 4-new_dog.c 2-print_dog.c 1-init_dog.c
