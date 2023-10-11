a5ae64bccad52cd01a7af5007de115e10d461c6d
emmanuel nnaji <realreach@yahoo.com>
2023-10-11 21:00:22 +0000

    fxn used function pointer to execute all actions in array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - fxn executes a function given as parameter
+ * @size: is the size of the array
+ * @action: is a pointer to the function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(*array);
+		array++;
+	}
+}
