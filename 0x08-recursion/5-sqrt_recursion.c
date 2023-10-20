bcda410ea4ba75a471a85eae551dc728056f3f03
emmanuel nnaji <realreach@yahoo.com>
2023-10-20 13:56:06 +0000

    fxn finds square root of integers

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,28 +1,32 @@
 #include "main.h"
-/**
- * _sqrt_recursion - fxn finds square root of number
- * @n: the number to be computed
- * description: the ethod used here is to continually subtract odd number
- * from the number n untill n becomes 0, the the resut becomes the number
- * iteration of the the process.
- * Return: return the result on success or -1 if not
- */
 
+int square_root_check(int n, int j);
+
+/**
+ * _sqrt_recursion - finds square root of number
+ * @n: the number to be checked
+ * Return: the result
+ */
 int _sqrt_recursion(int n)
 {
-	int static odd, count, result;
-
-	odd = 1;
-	count = result = 0;
-
-	if (n == 0 || n == 1)
-		return (n);
-	else if (n < 0)
+	if (n < 0)
 		return (-1);
 	else
-	{
-		result = ++count;
-		odd += 2;
-		_sqrt_recursion(n - odd);
-	}
+		return (square_root_check(n, 0));
+}
+
+/**
+ * square_root_check - recursively finds square-root of numbers
+ * description: i am a helper function to _sqrt_recursion
+ * @n: the numbe to be checked
+ * @j: the halve of n
+ * Return: result
+ */
+int square_root_check(int n, int j)
+{
+	if (j * j > n)
+		return (-1);
+	else if (j * j == n)
+		return (j);
+	return (square_root_check(n, j + 1));
 }
