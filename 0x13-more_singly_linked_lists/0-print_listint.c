f00d77ec80eba1e1dd5d61724e4ee43cbcd40dbb
emmanuel nnaji <realreach@yahoo.com>
2023-10-23 12:58:09 +0000

    add fxns that print and return number of list nodes

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_listint - fxn prints all lists of a linlked list
+ * @h: the given lists head pointer
+ * Return: the number of nodes in the list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+	const listint_t *ptr = h;
+
+	if (h == NULL)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	while (ptr->next)
+	{
+		count++;
+		printf("%d\n", ptr->n);
+		ptr = ptr->next;
+	}
+
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * listint_len - fxn return number of elements in a lists of a linlked list
+ * @h: the given lists head pointer
+ * Return: the number of nodes in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+	const listint_t *ptr = h;
+
+	if (h == NULL)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	while (ptr->next)
+	{
+		count++;
+		ptr = ptr->next;
+	}
+
+	return (count);
+}
