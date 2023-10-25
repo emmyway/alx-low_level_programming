c4b22e9764c763899f7be85618e64acaeb08748f
emmanuel nnaji <realreach@yahoo.com>
2023-10-25 03:14:41 +0000

    add fxn that  add node at begining of list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,1 +1,27 @@
-#include
+#include "lists.h"
+
+/**
+ * add_nodeint - fxn adds new node at begining of list
+ * @head: pointer to address of head so it could be passed bynreference
+ * @n: the data to be addeded to new node
+ *
+ * Return: pointer to new list
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *thenode;
+
+	if (head == NULL)/*check node validity*/
+		return (NULL);
+
+	thenode = malloc(sizeof(listint_t));
+	if (thenode == NULL)/*create anf validate new node*/
+		return (NULL);
+
+	thenode->n = n;
+
+	thenode->next = *head;
+	*head = thenode;
+
+	return (thenode);
+}
