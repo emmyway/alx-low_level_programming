f08d2da8fe4bf0cb379a75e221681eedf5540b17
emmanuel nnaji <realreach@yahoo.com>
2023-10-28 04:09:01 +0000

    fix bug

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -6,12 +6,14 @@
  * @index: the index
  * Return: 1 if success, -1 if error
  */
-int set_bit(unsigned long int *n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int mask;
+
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	unsigned long int mask = 1UL << index;
+	mask = 1UL << index;
 	*n = *n | mask;
 	return (1);
 }
