d501d9081329d716ba6b90c52404b1238ca615a2
emmanuel nnaji <realreach@yahoo.com>
2023-10-28 03:20:28 +0000

    fxn check endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness - fxn chexck of endianness
+ * Return: 0 if big endian, 1mif little endian
+ */
+int get_endianness(void)
+{
+	int i;
+	char *j;
+
+	i = 1;
+	j = (char *) & i;
+
+	return (*j);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,5 +9,6 @@ void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
 unsigned long int _pow(unsigned int base, unsigned int power);
+int get_endianness(void);
 
 #endif
