661bc884e33b615dfaf4f883bbb22f15f73356a6
emmanuel nnaji <realreach@yahoo.com>
2023-10-31 05:01:01 +0000

    debug

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY, | O_APPEND) 
+	fd = open(filename, O_WRONLY, | O_APPEND)
 	if (fd == -1)
 		return (-1);
 
@@ -24,7 +24,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	len = 0;
 	while (text_content[len])
 		len++;
-	
+
 	num_write = write(fd, text_content, len);
 
 	if (num_write == -1)
