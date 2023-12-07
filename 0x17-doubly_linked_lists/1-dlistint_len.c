6140e22eabb1781e4bb475cfad55cff2e8dab172
emmanuel nnaji <realreach@yahoo.com>
2023-12-07 15:52:50 +0000

    fxn print number of element of list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - prints the number of element of dlistint_t list
+ * @h: the head pointer to dlistint_t
+ * Return: number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *temp;
+	int num_elem = 0;
+
+	/* check if list is empty */
+	if (h == NULL)
+	{
+		dprintf(2, "list is empty\n");
+		exit(1);
+	}
+
+	/*assign iterating pointer*/
+	temp = h;
+
+	/*print elements of the list*/
+	while (temp)
+	{
+		num_elem += 1;
+		temp = temp->next;
+	}
+
+	return (num_elem);
+}
