f70614534fa84ec1a3e930125d3758a8cb722a38
emmanuel nnaji <realreach@yahoo.com>
2023-12-07 15:39:08 +0000

    fxn prints all elements of list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the element of dlistint_t list
+ * @h: the head pointer to dlistint_t
+ * Return: number of elements
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *temp;
+	int num_elem = 0;
+
+	/* check if list is empty */
+	if (h == NULL)
+	{
+		dprintf(2, "list is empty\n");
+		exit(1);
+	}
+
+	/*create and iterating poointer*/
+
+	/*assign iterating pointer*/
+	temp = h;
+
+	/*print elements of the list*/
+	while (temp)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		num_elem += 1;
+	}
+
+	return (num_elem);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,17 @@
+This is alx student course project, prepared to depeen our knowledge in programming Doubly linked list
+
+Here are the different tasks files:
+
+0-print_dlistint.c - prints all the data of a given list
+
+1-dlistint_len.c - 
+2-add_dnodeint.c 
+3-add_dnodeint_end.c 
+4-free_dlistint.c 
+5-get_dnodeint.c 
+6-sum_dlistint.c 
+7-insert_dnodeint.c 
+2-add_dnodeint.c 
+3-add_dnodeint_end.c -
+8-delete_dnodeint.c -
+lists.h -
