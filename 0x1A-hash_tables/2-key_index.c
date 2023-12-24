0da13ad9cbfd0612357a68a2e182bdcb9d26ca84
emmanuel nnaji <realreach@yahoo.com>
2023-12-24 16:58:01 +0000

    replace spaces in line initials with tabs, in my header file, to comply with betty

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,7 @@
+#include "hssh_tables.h"
+
+/**
+ * key_index - fxn returns index number from a hash key
+ * @key: the
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
