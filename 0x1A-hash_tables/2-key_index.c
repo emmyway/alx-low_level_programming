49adcf689f5a17a71faa348ff81e46a0208ccda1
emmanuel nnaji <realreach@yahoo.com>
2023-12-24 21:28:03 +0000

    fxn find hash index from key generated by djb2 algorithm

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,7 +1,16 @@
-#include "hssh_tables.h"
+#include "hash_tables.h"
 
 /**
  * key_index - fxn returns index number from a hash key
- * @key: the
+ * @key: the character string drive the key value
+ * @size: the size of the array
+ * Return: rhe index of tje key
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	index = hash_djb2(key) % size;
+
+	return (index);
+}
